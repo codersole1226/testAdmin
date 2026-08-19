c10ad34f191bcdb09989d120106958545e9a13f2
codersole <869645389@qq.com>
2026-08-19 14:31:51 +0800

    feat: 配置更改

--- src/main/java/com/codersole/knowledgeserver/config/WebConfig.java
+++ src/main/java/com/codersole/knowledgeserver/config/WebConfig.java
@@ -2,9 +2,7 @@ package com.codersole.knowledgeserver.config;
 
 import com.codersole.knowledgeserver.interceptor.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -29,5 +27,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**").addResourceLocations("file:uploads/");
+    }
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:5173")
+            .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS").allowedHeaders("*").allowCredentials(true);
     }
 }
